f91ae5e1864b2d79bfdfa1209169a9f9ecd657a1
kaushik <kaushikgupta67@gmail.com>
2021-08-26 21:29:02 +0530

    soups commit

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,17 @@ function fun()
     elmntToView.scrollIntoView(); 
 
 }
+
+function fun2()
+{
+    // alert("Hello");
+    var elmntToView = document.getElementById("soupsScroll");
+
+    if(elmntToView.style.display=="block")
+        elmntToView.style.display="none";
+    else
+        elmntToView.style.display="block";
+
+    elmntToView.scrollIntoView(); 
+
+}
